d56886578b6ae891ee0f47cbe0c43c85b948dcff
Ujjawal Kumar <ujjawalrr@gmail.com>
2024-09-05 23:17:39 +0530

    initial commit

--- convex/openai.ts
+++ convex/openai.ts
@@ -59,14 +59,3 @@ export const dall_e = action({
 		});
 	},
 });
-
-// 1 token ~= 4 chars in English
-// 1 token ~= ¾ words
-// 100 tokens ~= 75 words
-// Or
-// 1-2 sentence ~= 30 tokens
-// 1 paragraph ~= 100 tokens
-// 1,500 words ~= 2048 tokens
-
-// 1 image will cost $0,04(4 cents) => dall-e-3
-// 1 image will cost $0,02(2 cents) => dall-e-2
